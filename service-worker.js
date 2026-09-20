863114b869abd12edb3f69f765133b5afd78fbaf
carlos-dev97 <carlos.97fs@gmail.com>
2026-09-20 02:01:42 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,32 @@
 "use strict";
 
 const CACHE_NAME =
-    "gestao-equipa-horarios-v9-20260920";
+    "gestao-equipa-horarios-v10-20260920";
 
 const FILES_TO_CACHE = [
-
     "./",
-
     "./index.html",
-
     "./style.css?v=20260920",
-
     "./app.js?v=20260920",
-
-    "./manifest.json"
-
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
-
 
 self.addEventListener(
     "install",
     (event) => {
-
         event.waitUntil(
-
             caches
                 .open(CACHE_NAME)
-                .then(
-                    (cache) =>
-                        cache.addAll(
-                            FILES_TO_CACHE
-                        )
+                .then((cache) =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
                 )
-
         );
 
         self.skipWaiting();
-
     }
 );
 
@@ -44,39 +34,28 @@ self.addEventListener(
 self.addEventListener(
     "activate",
     (event) => {
-
         event.waitUntil(
-
             caches
                 .keys()
-                .then(
-                    (keys) =>
-
-                        Promise.all(
-
-                            keys
-                                .filter(
-                                    (key) =>
-                                        key !==
-                                        CACHE_NAME
+                .then((keys) =>
+                    Promise.all(
+                        keys
+                            .filter(
+                                (key) =>
+                                    key !==
+                                    CACHE_NAME
+                            )
+                            .map((key) =>
+                                caches.delete(
+                                    key
                                 )
-                                .map(
-                                    (key) =>
-                                        caches.delete(
-                                            key
-                                        )
-                                )
-
-                        )
-
+                            )
+                    )
                 )
-                .then(
-                    () =>
-                        self.clients.claim()
+                .then(() =>
+                    self.clients.claim()
                 )
-
         );
-
     }
 );
 
@@ -89,80 +68,55 @@ self.addEventListener(
             event.request.method !==
             "GET"
         ) {
-
             return;
-
         }
-
 
         const requestUrl =
             new URL(
                 event.request.url
             );
 
-
         if (
             requestUrl.origin !==
             self.location.origin
         ) {
-
             return;
-
         }
 
-
         event.respondWith(
+            fetch(event.request)
+                .then((response) => {
 
-            fetch(
-                event.request
-            )
-                .then(
-                    (response) => {
+                    if (
+                        response &&
+                        response.status ===
+                            200
+                    ) {
 
-                        if (
-                            response &&
-                            response.status === 200 &&
-                            response.type !== "opaque"
-                        ) {
+                        const clone =
+                            response.clone();
 
-                            const clone =
-                                response.clone();
-
-                            caches
-                                .open(CACHE_NAME)
-                                .then(
-                                    (cache) => {
-
-                                        cache.put(
-                                            event.request,
-                                            clone
-                                        );
-
-                                    }
-                                );
-
-                        }
-
-                        return response;
-
-                    }
-                )
-                .catch(
-                    () =>
                         caches
-                            .match(
-                                event.request
+                            .open(
+                                CACHE_NAME
                             )
                             .then(
-                                (cached) =>
-                                    cached ||
-                                    caches.match(
-                                        "./index.html"
-                                    )
-                            )
+                                (cache) => {
+                                    cache.put(
+                                        event.request,
+                                        clone
+                                    );
+                                }
+                            );
+                    }
+
+                    return response;
+                })
+                .catch(() =>
+                    caches.match(
+                        event.request
+                    )
                 )
-
         );
-
     }
 );
